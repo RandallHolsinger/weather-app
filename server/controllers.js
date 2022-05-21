5cb3e837abad713f1cc12068266da2d83a55a49f
Randall Holsinger <randallholsinger@gmail.com>
2022-05-20 17:34:59 -0700

    deleted api

--- server/controllers.js
+++ server/controllers.js
@@ -23,46 +23,4 @@ module.exports = {
             console.log({errorMessage: `${err}`})
         }
     },
-
-    fiveday: async (req, res) => {
-        const {lat, lon} = req.params
-        console.log('hitting backend 5day', lat, lon)
-        try {
-            await axios({
-                method: 'GET',
-                url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly',
-                params: {lat: `${lat}`, lon: `${lon}`},
-                headers: {
-                  'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com',
-                  'X-RapidAPI-Key': '2081668b64msh13cc62be7f14c36p1bbdb0jsn007f29176bd7'
-                }
-            })
-            .then(response => {
-                res.send(response.data)
-            })
-        } catch(err) {
-            console.log({errorMessage: `${err}`})
-        }
-    },
-
-    sixteenday: async (req, res) => {
-        const {lat, lon} = req.params
-        console.log(lat, lon)
-        try {
-            await axios({
-                method: 'GET',
-                url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily',
-                params: {lat: `${lat}`, lon: `${lon}`},
-                headers: {
-                  'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com',
-                  'X-RapidAPI-Key': '2081668b64msh13cc62be7f14c36p1bbdb0jsn007f29176bd7'
-                }
-            })
-            .then(response => {
-                res.send(response.data)
-            })
-        } catch(err) {
-            console.log({errorMessage: `${err}`})
-        }
-    }
 }
