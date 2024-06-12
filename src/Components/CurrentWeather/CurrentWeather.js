6b88c220d5f4c9260eaaff5979a1c1c105a2c4ee
Randall Holsinger <randallholsinger@gmail.com>
2024-06-12 12:02:02 -0700

    added more details to daily details

--- src/Components/CurrentWeather/CurrentWeather.js
+++ src/Components/CurrentWeather/CurrentWeather.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import './CurrentWeather.css'
 import { RotatingLines } from 'react-loader-spinner'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp, faArrowDown } from '@fortawesome/fontawesome-free-solid'
 import WeatherIcon from '../WeatherIcon/WeatherIcon'
 
 function CurrentWeather(props) {
@@ -28,9 +30,11 @@ function CurrentWeather(props) {
               <div className='current-weather-container'>
                 <div className='current-weather-info'>
                   <div className="temp-desc">
+                    <h3>{weatherData.location.city},{' '}{weatherData.location.region}</h3>
                     <h2>{weatherData.current_observation.condition.temperature}°</h2>
                     <h4>{weatherData.current_observation.condition.text}</h4>
-                    <h4>High:{' '}{weatherData.forecasts[0].high}{weatherUnit === 'f' ? '°F' : '°C'}{' '}•{' '}Low:{' '}{weatherData.forecasts[0].low}{weatherUnit === 'f' ? '°F' : '°C'}</h4>
+                    <h4>
+                      <FontAwesomeIcon icon={faArrowUp} />{' '}High:{' '}{weatherData.forecasts[0].high}{weatherUnit === 'f' ? '°F' : '°C'}{' '}•{' '}<FontAwesomeIcon icon={faArrowDown} />{' '}Low:{' '}{weatherData.forecasts[0].low}{weatherUnit === 'f' ? '°F' : '°C'}</h4>
                   </div>
                   <div className="current-weather-icon">
                     <WeatherIcon code={weatherData.current_observation.condition.code} />
